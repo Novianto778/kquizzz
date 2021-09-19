10d2ac8516ba885a99e17da742c4a5d1d01ef701
Novianto <novainto778@gmail.com>
2021-09-19 21:55:00 +0700

    Heroku Fix

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const db = require('./utils/database');
 
 const { loadQuiz, findQuiz } = require('./utils/quiz');
